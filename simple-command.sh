efc65643da9cb03f007a3f2a62106d28b3aa1875
Scott Cain <scott+git@scottcain.net>
2021-04-06 18:47:52 +0000

    trying to figure out why only a few commands in the script exec

--- simple-command.sh
+++ simple-command.sh
@@ -2,12 +2,7 @@
 
 cd /jbrowse
 
-wget http://zfin.org/downloads/E_antibody.gff3
-
-
-bin/flatfile-to-json.pl --gff E_antibody.gff3 --type protein_coding_gene --key "ZFIN Genes with Antibody Data" --trackType CanvasFeatures --trackLabel "ZFIN Genes with Antibody Data" --compress
-
-bin/generate-names.pl --compress
+wget http://zfin.org/downloads/E_antibody.gff3; bin/flatfile-to-json.pl --gff E_antibody.gff3 --type protein_coding_gene --key "ZFIN Genes with Antibody Data" --trackType CanvasFeatures --trackLabel "ZFIN Genes with Antibody Data" --compress; bin/generate-names.pl --compress
 
 
 #the upload_to_S3.pl script is in the agr_jbrowse_config repo
