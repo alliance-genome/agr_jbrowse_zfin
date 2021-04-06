82cbdcab8b85f51786e04257f7102620ccde2de8
Scott Cain <scott+git@scottcain.net>
2021-04-06 18:11:17 +0000

    simple testing script for processing (no upload yet)

---
+++ simple-command.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+wget http://zfin.org/downloads/E_antibody.gff3
+
+
+bin/flatfile-to-json.pl --gff E_antibody.gff3 --type protein_coding_gene --key "ZFIN Genes with Antibody Data" --trackType CanvasFeatures --trackLabel "ZFIN Genes with Antibody Data" --compress
+
+bin/generate-names.pl --compress
+
+
+#the upload_to_S3.pl script is in the agr_jbrowse_config repo
+#this command is run in a jbrowse/data directory
+#~/scain/agr_jbrowse_config/scripts/upload_to_S3.pl --bucket agrjbrowse --local . --remote MOD-jbrowses/zfin --skipseq
+
+# s3://agrjbrowse/MOD-jbrowses/zfin/
+
