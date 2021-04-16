53d6bcf9c6d74f1f7acfa70236fa032c39cfb202
Scott Cain <scott+git@scottcain.net>
2021-04-16 18:39:28 +0000

    switching over to using environment variables for processing script

--- simple-command.sh
+++ simple-command.sh
@@ -1,5 +1,26 @@
 #!bin/sh
 
+if [ -z "$1" ]
+then
+    AWSACCESS=${AWS_ACCESS_KEY}
+else
+    AWSACCESS=$1
+fi
+
+if [ -z "$2" ]
+then
+    AWSSECRET=${AWS_SECRET_KEY}
+else
+    AWSSECRET=$2
+fi
+
+if [ -z ${AWS_S3_BUCKET} ]
+then
+    AWSBUCKET=agrjbrowse
+else
+    AWSBUCKET=${AWS_S3_BUCKET}
+fi
+
 cd /jbrowse
 
 wget http://zfin.org/downloads/E_antibody.gff3; bin/flatfile-to-json.pl --gff E_antibody.gff3 --type protein_coding_gene --key "ZFIN Genes with Antibody Data" --trackType CanvasFeatures --trackLabel "ZFIN Genes with Antibody Data" --compress; bin/generate-names.pl --compress
@@ -40,7 +61,7 @@ rm zfin_mutants.gff3
 
 #the upload_to_S3.pl script is in the agr_jbrowse_config repo
 #this command is run in a jbrowse directory
-/agr_jbrowse_config/scripts/upload_to_S3.pl --bucket agrjbrowse --local data --remote MOD-jbrowses/zfin --skipseq --awsaccess $1 --awssecret $2
+/agr_jbrowse_config/scripts/upload_to_S3.pl --bucket $AWSBUCKET --local data --remote MOD-jbrowses/zfin --skipseq --awsaccess $AWSACCESS --awssecret $AWSSECRET
 
 # s3://agrjbrowse/MOD-jbrowses/zfin/
 
