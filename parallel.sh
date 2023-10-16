4088c67c08c49eba76fe4521c9f1172d77d1ad50
Scott Cain <scott+git@scottcain.net>
2023-10-16 19:41:35 +0000

    throw away changes to load requirements (for testing)

--- parallel.sh
+++ parallel.sh
@@ -67,9 +67,9 @@ LABELS=(
 'ZFIN_Features'
 )
 
-parallel -j "96%" wget -q  http://zfin.org/downloads/{} 2>&1 ::: "${FILES[@]}"
+parallel -j "33%" wget -q  http://zfin.org/downloads/{} 2>&1 ::: "${FILES[@]}"
 
-parallel --link -j "96%"  bin/flatfile-to-json.pl --trackType CanvasFeatures  --compress --gff {1} --type {2} --trackLabel {3} ::: "${FILES[@]}" ::: "${TYPES[@]}" ::: "${LABELS[@]}"
+parallel --link -j "33%"  bin/flatfile-to-json.pl --trackType CanvasFeatures  --compress --gff {1} --type {2} --trackLabel {3} ::: "${FILES[@]}" ::: "${TYPES[@]}" ::: "${LABELS[@]}"
 
 #start track uploads while running the name indexer
 for label in "${LABELS[@]}"; do
